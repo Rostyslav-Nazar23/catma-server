22f92479763b3cce734b969250099635351b0887
Rostyslav-Nazar23 <rostislav.nazarenko23@gmail.com>
2024-11-28 20:45:25 +0200

    Starting code cleanup Dependencies moved to toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-val exposed_version: String by project
-val h2_version: String by project
-val koin_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     kotlin("jvm") version "2.0.21"
-    id("io.ktor.plugin") version "3.0.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.rnazarenko.catma.server"
@@ -26,28 +20,30 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-sessions-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
-    implementation("io.ktor:ktor-server-websockets-jvm")
-    implementation("io.github.flaxoos:ktor-server-task-scheduling-core:2.1.1")
-    implementation("io.github.flaxoos:ktor-server-task-scheduling-redis:2.1.1")
-    implementation("io.github.flaxoos:ktor-server-task-scheduling-mongodb:2.1.1")
-    implementation("io.github.flaxoos:ktor-server-task-scheduling-jdbc:2.1.1")
-    implementation("io.ktor:ktor-server-compression-jvm")
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-server-host-common-jvm")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml-jvm")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // exposed libs
+    implementation(libs.bundles.exposed)
+
+    // ktor server task scheduling
+    implementation(libs.bundles.ktor.task.scheduling)
+
+    // ktor server
+    implementation(libs.bundles.ktor.server)
+    testImplementation(libs.ktor.server.test.host)
+
+    // serialization
+    implementation(libs.ktor.serilization)
+
+    // h2 database
+    implementation(libs.h2.database)
+
+    // koin
+    implementation(libs.bundles.koin)
+
+    // qos logback
+    implementation(libs.qos.logback)
+
+    // kotlin test unit
+    testImplementation(libs.kotlin.test.unit)
+
 }
--- src/main/kotlin/com/rnazarenko/catma/server/HTTP.kt
+++ src/main/kotlin/com/rnazarenko/catma/server/HTTP.kt
@@ -4,7 +4,6 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.cors.routing.*
-//import io.ktor.server.response.*
 
 fun Application.configureHTTP() {
     install(Compression)
